79822357dcbaffd67ada48c41186b4ad2fd710cb
Kush Patel <kush@kush.in>
2023-06-14 01:15:23 +0530

    fixed imports, env management

--- src/analysis/processing_data.py
+++ src/analysis/processing_data.py
@@ -1,9 +1,12 @@
 import pandas as pd
+
 from src.data.news_trends import get_sorted_news
+
 
 def get_news_df():
     sorted_news = get_sorted_news()
-    df = pd.DataFrame(sorted_news.items(), columns=['title', 'content'])
+    df = pd.DataFrame(sorted_news.items(), columns=["title", "content"])
     return df
+
 
 get_news_df()
