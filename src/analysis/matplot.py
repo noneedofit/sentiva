45065c9d982eb5ef4f7acaf6ad024b7a8419c9e2
MonaLisaisgay <notsoharsh1809@gmail.com>
2023-06-16 21:04:40 +0530

    ok

--- src/analysis/matplot.py
+++ src/analysis/matplot.py
@@ -19,12 +19,12 @@ def plot_sentiment():
     ax.set_facecolor("white")
     ax.plot(df['sentiment_score'])
     
-    cmap = plt.get_cmap('plasma',20)
+    cmap = plt.get_cmap('plasma',10)
     norm = mpl.colors.Normalize(vmin=0,vmax=2)
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
-    plt.colorbar(sm, ticks=np.linspace(-1,1,100), 
-             boundaries=np.arange(-0.05,2.1,.1))
+    plt.colorbar(sm, ticks=(-1.0, -0.95, -0.9, -0.85, -0.8, -0.75, -0.7, -0.65, -0.6, -0.55, -0.5, -0.45, -0.4, -0.35, -0.3, -0.25, -0.2, -0.15, -0.1, -0.05, 0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0), 
+                 boundaries=np.arange(-1,2.1,.1))
 
     plt.show()
 
