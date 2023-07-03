27bfe02ccc794568b86fa9b719adb196af59c6e0
Dhruv <officialdhruvinc@gmail.com>
2023-07-03 15:32:04 +0530

    changes to the zero score updating. console logging & error handling in main.py.

--- main.py
+++ main.py
@@ -1,74 +1,87 @@
-# collect data from API and get news dataframes 
-
 from src.data.top_headlines import get_sorted_top_headlines
 from src.data.category_news import get_categorised_news
 from src.data.news_dataframe import get_top_headlines_df, get_categorised_news_df
+from src.analysis.sent_analysis import get_sentiment_df, count_zero_sentiment, update_zero_sentiment
+import time
 
-# get sentiment scores and plot sentiment scores
+def print_status(text, status):
+    print(f"{text.ljust(45)} {status}")
 
-from src.analysis.sent_analysis import get_sentiment_df
-from src.analysis.sent_analysis import count_zero_sentiment
-from src.analysis.sent_analysis import update_zero_sentiment
-from src.analysis.sent_plots import plot_sentiment
+def print_error(text, error):
+    print(f"{text.ljust(45)} ERROR: {error}")
 
+def main(df_stats=False, updated_stats=False):
+   try:
+        # get data from API
+        print_status('Getting articles from the API..', '🟡')
+        time.sleep(1)
+        sorted_news = get_sorted_top_headlines()
+        categorised_news = get_categorised_news()
+        print_status('Fetching DONE', '✅')
 
-def main():
+        # get dataframes from data 
+        print_status('Making dataframes for the articles..', '🟡')
+        time.sleep(1)
+        top_headlines_df = get_top_headlines_df(sorted_news)
+        categorised_news_df = get_categorised_news_df(categorised_news)
+        print_status('Dataframes MADE', '✅')
 
-    # get data from API
-    print('Getting articles from the API..')
-    sorted_news = get_sorted_top_headlines()
-    categorised_news = get_categorised_news()
-    print('Fetching DONE.')
+        # get sentiment scores for each article 
+        print_status('Applying sentiment analysis on the articles..', '🟡')
+        time.sleep(1)
+        top_headlines_df = get_sentiment_df(top_headlines_df)
+        categorised_news_df = get_sentiment_df(categorised_news_df)
+        print_status('Analysis DONE', '✅')
 
-    # get dataframes from data 
-    print('Making dataframes for the articles..')
-    top_headlines_df = get_top_headlines_df(sorted_news)
-    categorised_news_df = get_categorised_news_df(categorised_news)
-    print('Dataframes MADE.')
+        # update sentiment scores for articles with 0 sentiment score
+        print_status('Records with ZERO score information..', '🟡')
+        time.sleep(1)
+        top_headlines_df = update_zero_sentiment(top_headlines_df)
+        categorised_news_df = update_zero_sentiment(categorised_news_df)
+        print_status('Records UPDATED', '✅')
 
-    # get sentiment scores for each article 
-    print('Applying sentiment analysis on the articles..')
-    top_headlines_df = get_sentiment_df(top_headlines_df)
-    categorised_news_df = get_sentiment_df(categorised_news_df)
-    print('Analysis DONE.')
+        # plot sentiment scores
+        print_status('Visualising our data..', '🟡')
+        time.sleep(1)
+        '''
+        plot_sentiment(top_headlines_df, "Trending News")
+        plot_sentiment(categorised_news_df, "Categorised News")
+        '''
+        print_status('Visualisation DONE', '✅')
 
-    # update sentiment scores for articles with 0 sentiment score
-    print('Records with ZERO score information..')
-    top_headlines_df = update_zero_sentiment(top_headlines_df)
-    categorised_news_df = update_zero_sentiment(categorised_news_df)
-    print('Number of 0 -> non-zero sentiment score updates: ', top_headlines_df.size + categorised_news_df.size - 2*count_zero_sentiment(top_headlines_df).size - 2*count_zero_sentiment(categorised_news_df).size)
+   except Exception as e:
+        print_error('An error occurred:', str(e))
 
-    # plot sentiment scores
-    print('Visualising our data..')
-    '''
-    plot_sentiment(top_headlines_df, "Trending News")
-    plot_sentiment(categorised_news_df, "Categorised News")
-    '''
+        if df_stats:
+           # dataframes sizes
+           print('Number of articles in TOP HEADLINES dataframe: ', top_headlines_df.size)
+           print('Number of articles in CATEGORISED news dataframe: ', categorised_news_df.size)
+        
+           print('Number of TRENDING articles with ZERO in sentiment score: ', count_zero_sentiment(top_headlines_df).size)
+           print('Number of CATEGORISED articles with ZERO in sentiment score: ', count_zero_sentiment(categorised_news_df).size)
 
-    # dataframes sizes
-    print('Number of articles in TOP HEADLINES dataframe: ', top_headlines_df.size)
-    print('Number of articles in CATEGORISED news dataframe: ', categorised_news_df.size)
-    
-    print('Number of TRENDING articles with ZERO in sentiment score: ', count_zero_sentiment(top_headlines_df).size)
-    print('Number of CATEGORISED articles with ZERO in sentiment score: ', count_zero_sentiment(categorised_news_df).size)
+           # print dataframes
+           print('TOP HEADLINES dataframe: ')
+           print(top_headlines_df.head(10))
 
-    # print dataframes
-    print('TOP HEADLINES dataframe: ')
-    print(top_headlines_df.head(50))
+           print('CATEGORISED news dataframe: ')
+           print(categorised_news_df.head(10))
 
-    print('CATEGORISED news dataframe: ')
-    print(categorised_news_df.head(50))
+        elif updated_stats:
+           # updated 0 -> non zero scores df and also retrieve the no. of records updated in each df
+           updated_top_headlines_df, updated_top_count = update_zero_sentiment(top_headlines_df, True, True)
+           updated_categorised_news_df, updated_categorised_count = update_zero_sentiment(categorised_news_df, True, True)
 
-    # updated 0 -> non zero scores df
-    updated_top_headlines_df = update_zero_sentiment(top_headlines_df, True)
-    updated_categorised_news_df = update_zero_sentiment(categorised_news_df, True)
+           # print no. of updated records 
+           print('No. of records updated in TOP HEADLINES dataframe: ', updated_top_count)
+           print('No. of records updated in CATEGORISED news dataframe: ', updated_categorised_count)
+           print('TOTAL NO. OF RECORDS UPDATED with ZERO score: ', updated_top_count + updated_categorised_count)
 
-    print('UPDATED TOP HEADLINES dataframe: ', updated_top_headlines_df.size)
-    print(updated_top_headlines_df.head(50))
+           print('UPDATED TOP HEADLINES dataframe: ')
+           print(updated_top_headlines_df.head(15))
 
-    print('UPDATED CATEGORISED news dataframe: ', updated_categorised_news_df.size)
-    print(updated_categorised_news_df.head(50))
-
+           print('UPDATED CATEGORISED news dataframe: ')
+           print(updated_categorised_news_df.head(15))
 
 if __name__ == "__main__":
     main()
--- src/analysis/sent_analysis.py
+++ src/analysis/sent_analysis.py
@@ -1,4 +1,5 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import numpy as np
 
 analyzer = SentimentIntensityAnalyzer()
 
@@ -14,10 +15,11 @@ def get_sentiment_df(df):
     return df
     
 
-def update_zero_sentiment(df, return_updated_records=False):
+def update_zero_sentiment(df, return_updated_records=False, zero_sentiment_updated=False):
     updated_df = df.copy()  # create a new DataFrame to store the updated records
     zero_sentiment_count = df[df['sentiment_score'] == 0].shape[0]  # number of records with 0 sentiment score
-    
+    updated_count = 0  # variable to track the number of records updated
+
     while True:
         zero_sentiment_df = updated_df[updated_df['sentiment_score'] == 0]
         
@@ -29,6 +31,7 @@ def update_zero_sentiment(df, return_updated_records=False):
             new_sentiment_score = get_sentiment_score(row["content"])
             updated_df.at[index, "sentiment_score"] = new_sentiment_score
             updated_df.at[index, "original_sentiment_score"] = original_sentiment_score
+            updated_count += 1
         
         new_zero_sentiment_count = updated_df[updated_df['sentiment_score'] == 0].shape[0]
         
@@ -38,12 +41,18 @@ def update_zero_sentiment(df, return_updated_records=False):
         zero_sentiment_count = new_zero_sentiment_count
     
     if return_updated_records:
-        return updated_df
+        if zero_sentiment_updated:
+            return updated_df, updated_count
+        else:
+            return updated_df
     else:
-        return df
-
-
+        if zero_sentiment_updated:
+            return df, updated_count
+        else:
+            return df
+        
+    
 def count_zero_sentiment(df):
-    # count the number of articles with a sentiment score of 0.
-    zero_sentiment_df = df[df['sentiment_score'] == 0]
+    # count the number of articles with a sentiment score of 0, NaN, or None.
+    zero_sentiment_df = df[df['sentiment_score'].isin([0, np.nan, None])]
     return zero_sentiment_df
